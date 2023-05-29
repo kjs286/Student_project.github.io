6e3bf2f9da6d12152166fa22259a6b4a9b0f40e5
Jayendra Kanzariya <kanzariyajayendra786@gmail.com>
2023-05-30 03:27:48 +0530

    Add project files.

---
+++ Student_project/StudentsForm.aspx
@@ -0,0 +1,97 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="StudentsForm.aspx.cs" Inherits="Student_project.StudentsForm" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Form</title>
+    <style>
+        .form-container {
+            max-width: 400px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #f5f5f5;
+            border: 1px solid #ddd;
+            border-radius: 5px;
+        }
+
+        .form-field {
+            margin-bottom: 10px;
+        }
+
+        .form-label {
+            display: block;
+            font-weight: bold;
+        }
+
+        .form-input {
+            width: 100%;
+            padding: 8px;
+            border: 1px solid #ddd;
+            border-radius: 4px;
+        }
+
+        .form-submit {
+            margin-top: 10px;
+        }
+
+        .form-table {
+            width: 100%;
+            margin-top: 20px;
+            border-collapse: collapse;
+        }
+
+            .form-table th,
+            .form-table td {
+                padding: 8px;
+                border: 1px solid #ddd;
+            }
+
+        .form-download-link {
+            display: block;
+            margin-top: 10px;
+        }
+    </style>
+</head>
+<body>
+    <div class="form-container">
+        <form runat="server" enctype="multipart/form-data">
+            <div class="form-field">
+                <label for="studentName" class="form-label">Name of the Student:</label>
+                <asp:TextBox ID="studentName" runat="server" CssClass="form-input" required></asp:TextBox>
+            </div>
+            <div class="form-field">
+                <label for="instituteName" class="form-label">Institute Name:</label>
+                <asp:TextBox ID="instituteName" runat="server" CssClass="form-input" required></asp:TextBox>
+            </div>
+            <div class="form-field">
+                <label for="courseName" class="form-label">Course Name:</label>
+                <asp:TextBox ID="courseName" runat="server" CssClass="form-input" required></asp:TextBox>
+            </div>
+            <div class="form-field">
+                <label for="courseStartDateTime" class="form-label">Course Start Date and Time:</label>
+                <asp:TextBox ID="courseStartDateTime" runat="server" placeholder="mm/dd/yyyy hh:mm:ss" TextMode="DateTimeLocal" CssClass="form-input" required></asp:TextBox>
+            </div>
+            <div class="form-field">
+                <label for="pdfFile" class="form-label">Upload PDF:</label>
+                <asp:FileUpload ID="pdfFile" runat="server" CssClass="form-input" required></asp:FileUpload>
+            </div>
+            <asp:Button ID="Submit" runat="server" Text="Submit" CssClass="form-submit" OnClick="submitButton_Click" />
+            <asp:GridView ID="dataGridView" runat="server" CssClass="form-table" AutoGenerateColumns="false">
+                <Columns>
+                    <asp:BoundField DataField="Name" HeaderText="Student Name" />
+                    <asp:BoundField DataField="Institute" HeaderText="Institute Name" />
+                    <asp:BoundField DataField="Course" HeaderText="Course Name" />
+                    <asp:BoundField DataField="StartDateTime" HeaderText="Course Start Date and Time" />
+                    <asp:TemplateField HeaderText="Download PDF">
+                        <ItemTemplate>
+                            <asp:LinkButton ID="Download" runat="server" Text="Download PDF" CssClass="form-download-link"
+                                OnCommand="downloadLink_Command" CommandArgument='<%# Eval("StudentId") %>'></asp:LinkButton>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                </Columns>
+            </asp:GridView>
+        </form>
+
+    </div>
+</body>
+</html>
---
+++ Student_project/StudentsForm.aspx.cs
@@ -0,0 +1,189 @@
+﻿using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ZXing;
+using ZXing.Common;
+using Image = iTextSharp.text.Image;
+
+namespace Student_project
+{
+    public partial class StudentsForm : System.Web.UI.Page
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["MyConn"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // Bind the GridView with data on page load
+                BindGridView();
+            }
+        }
+
+        protected void submitButton_Click(object sender, EventArgs e)
+        {
+                // Save the uploaded PDF file to the server
+                string fileName = Path.GetFileName(pdfFile.FileName);
+                string filePath = Server.MapPath("~/Uploads/" + fileName);
+                pdfFile.SaveAs(filePath);
+
+                // Insert the form data into the database
+                InsertFormData(studentName.Text, instituteName.Text, courseName.Text, Convert.ToDateTime(courseStartDateTime.Text), fileName);
+
+                // Bind the GridView with updated data
+                BindGridView();
+
+                // Clear the form fields
+                ClearForm();
+        }
+
+        protected void downloadLink_Command(object sender, CommandEventArgs e)
+        {
+            // Retrieve the PDF file name based on the command argument (ID)
+            int studentId = Convert.ToInt32(e.CommandArgument);
+            string fileName = GetFileNameFromId(studentId);
+
+            if (!string.IsNullOrEmpty(fileName))
+            {
+                // Download the PDF file
+                // Generate QR code using student ID
+                string filePath = Server.MapPath("~/Uploads/" + fileName);
+                string qrCodeContent = $"Student ID: {studentId}";
+
+                string qrCodeImagePath = Server.MapPath("~/QRCodes/" + studentId + ".png");
+                GenerateQRCode(qrCodeContent, qrCodeImagePath);
+
+                // Modify the uploaded PDF by placing the QR code
+                string modifiedPdfPath = Server.MapPath("~/ModifiedPDFs/" + fileName);
+                ModifyPdfWithQRCode(filePath, qrCodeImagePath, modifiedPdfPath);
+
+                // Download the modified PDF
+                Response.Clear();
+                Response.ContentType = "application/pdf";
+                Response.AppendHeader("Content-Disposition", "attachment; filename=" + fileName);
+                Response.TransmitFile(modifiedPdfPath);
+                Response.End();
+            }
+        }
+
+        protected void GenerateQRCode(string content, string imagePath)
+        {
+            BarcodeWriter barcodeWriter = new BarcodeWriter();
+            barcodeWriter.Format = BarcodeFormat.QR_CODE;
+
+            EncodingOptions encodingOptions = new EncodingOptions();
+            encodingOptions.Height = 300;
+            encodingOptions.Width = 300;
+
+            barcodeWriter.Options = encodingOptions;
+            Bitmap bitmap = barcodeWriter.Write(content);
+
+            bitmap.Save(imagePath);
+        }
+
+        protected void ModifyPdfWithQRCode(string inputPdfPath, string qrCodeImagePath, string outputPdfPath)
+        {
+            using (FileStream fs = new FileStream(inputPdfPath, FileMode.Open, FileAccess.Read))
+            {
+                PdfReader reader = new PdfReader(fs);
+                using (FileStream outStream = new FileStream(outputPdfPath, FileMode.Create, FileAccess.Write))
+                {
+                    PdfStamper stamper = new PdfStamper(reader, outStream);
+                    PdfContentByte content = stamper.GetOverContent(1);
+
+                    Image qrCodeImage = Image.GetInstance(qrCodeImagePath);
+                    qrCodeImage.ScaleAbsolute(80, 90);
+                    qrCodeImage.SetAbsolutePosition(135, 50);
+                    content.AddImage(qrCodeImage);
+
+                    stamper.Close();
+                }
+                reader.Close();
+            }
+        }
+
+        private void BindGridView()
+        {
+            DataTable dt = GetFormData();
+
+            // Bind the DataTable to the GridView
+            dataGridView.DataSource = dt;
+            dataGridView.DataBind();
+        }
+
+        private DataTable GetFormData()
+        {
+            DataTable dt = new DataTable();
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string query = "SELECT StudentId, Name, Institute, Course, StartDateTime, FileName  FROM StudentTable";
+                SqlCommand cmd = new SqlCommand(query, con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+                con.Open();
+                da.Fill(dt);
+            }
+
+            return dt;
+        }
+
+        private void InsertFormData(string studentName, string instituteName, string courseName, DateTime courseStartDateTime, string fileName)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string query = "INSERT INTO StudentTable (Name, Institute, Course, StartDateTime, FileName) " +
+                               "VALUES (@Name, @Institute, @Course, @StartDateTime, @FileName)";
+                SqlCommand cmd = new SqlCommand(query, con);
+
+                // Add parameters to the SqlCommand
+                cmd.Parameters.AddWithValue("@Name", studentName);
+                cmd.Parameters.AddWithValue("@Institute", instituteName);
+                cmd.Parameters.AddWithValue("@Course", courseName);
+                cmd.Parameters.AddWithValue("@StartDateTime", courseStartDateTime);
+                cmd.Parameters.AddWithValue("@FileName", fileName);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        private string GetFileNameFromId(int id)
+        {
+            string fileName = string.Empty;
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string query = "SELECT FileName FROM StudentTable WHERE StudentId = @Id";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@Id", id);
+
+                con.Open();
+                object result = cmd.ExecuteScalar();
+
+                if (result != null)
+                {
+                    fileName = result.ToString();
+                }
+            }
+
+            return fileName;
+        }
+
+        private void ClearForm()
+        {
+            studentName.Text = string.Empty;
+            instituteName.Text = string.Empty;
+            courseName.Text = string.Empty;
+            courseStartDateTime.Text = string.Empty;
+        }
+    }
+}
